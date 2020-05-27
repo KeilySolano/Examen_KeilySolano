2e75783bc0410550332eb8e5c9de99b1212156dd
KeilySolano <62618755+KeilySolano@users.noreply.github.com>
2020-05-27 12:59:30 -0600

    Cambio01

---
+++ Usuario.php
@@ -0,0 +1,21 @@
+<?php
+            include ('ValidarUsuario.php');
+
+            $Validere = new ValidarUsuario;
+
+            $Nombre =$_POST['Nombre'];
+            $Password =$_POST['Password'];
+
+            $key = $Validere->ValidarBruker($Nombre, $Password);
+
+            var_dump($key);
+
+         if($key ->num_rows == 1 )
+         {
+              header("location:Agregar.php");
+         }
+         else 
+         {
+              header("location:Index.html");
+         }
+?>
