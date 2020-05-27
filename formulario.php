ce4a009a2d91e3d48a6e91310d1ca7f5afa90f24
KeilySolano <62618755+KeilySolano@users.noreply.github.com>
2020-05-27 15:31:31 -0600

    Cambio05

--- formulario.php
+++ formulario.php
@@ -1,33 +1,21 @@
-<!DOCTYPE>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Agregar Usuario</title>
 </head>
 <body>
 	<?php 
-		$Server = "localhost";
-	    $Datebase = "usuario";
-        $Usuarios = "root";
-        $Contraseña = "";	
 
-		$conexion=mysqli_connect($Server,$Usuarios,$Contraseña,$Datebase)
-			or die("Error al conectar");
-
-		$Nombre = $_POST['txtNombre'];
+		include('Conexion.php');
+	    $con = new Conexion();
+        $Nombre = $_POST['txtNombre'];
 		$Apellido = $_POST['txtApellido'];
 		$Usuario = $_POST['txtUsuario'];
 		$Password = $_POST['txtPassword'];
 
-		
-		$insertar = "INSERT into usuario values ('$Nombre','$Apellido','$Usuario','$Password')";
-
-		$resultado = mysqli_query($conexion,$insertar)
-			or die("Error al conectar");
-
-		mysqli_close($conexion);
-		echo "Datos ingresados";
-
-		header("location:Index.html");
+        $query="INSERT INTO `usuario`( `Nombre`, `Apellido`, `Usuario`, `Password`) VALUES ('$Nombre','$Apellido','$Usuario','$Password');";                
+        $consulta=$con->query($query);
+        $con->close();						
 	?>
 </body>
 </html>
