653d9def3b28ed9ba249b5f052f90585029b5168
KeilySolano <62618755+KeilySolano@users.noreply.github.com>
2020-05-27 14:49:02 -0600

    Cambio02

--- Agregar.php
+++ Agregar.php
@@ -1,1 +1,16 @@
-﻿
+﻿<!DOCTYPE>
+<html>
+<head>
+	<title>Agregar Usuario</title>
+	<link rel="stylesheet" type="text/css" href="Estilos.css" />
+</head>
+<body>
+	<form action="formulario.php" method="POST">
+		Nombre:<input type="text" name="txtNombre">;
+		Apellido:<input type="text" name="txtApellido">;
+		Usuario:<input type="text" name="txtUsuario">;
+		Contraseña:<input type="text" name="txtPassword">;
+		<input type="submit" value="formulario" name="Registrar">
+	</form>
+</body>
+</html>
